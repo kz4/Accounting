5ced68070ea2b70d6e8f6ac807dd9894d8992dd9
kz4 <zkcneu@gmail.com>
2019-08-01 19:34:43 -0400

    Search text intangible instead and scrap the whole page

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 ## Prerequisite
 Python, Selenium and BeautifulSoup
 ## Objective
-This scraps the Intangible Assets table from https://www.sec.gov/cgi-bin/viewer?action=view&cik=1018724&accession_number=0001018724-19-000004&xbrl_type=v#
-Navigate -> Notes to Financial Statements -> Acquisitions, Goodwill, and Acquired Intangible Assets
+This scraps the Intangible Assets page from SEC. 
+1. Amazon
+https://www.sec.gov/cgi-bin/viewer?action=view&cik=1018724&accession_number=0001018724-19-000004&xbrl_type=v#
+Navigate -> Notes to Financial Statements -> Acquisitions, Goodwill, and Acquired Intangible Assets - whole thing
+2. Microsoft
+https://www.sec.gov/cgi-bin/viewer?action=view&cik=789019&accession_number=0001564590-18-019062&xbrl_type=v# 
+Notes to Financial Statements - Intangible Assets - whole thing
+3. Coach
+https://www.sec.gov/cgi-bin/viewer?action=view&cik=1116132&accession_number=0001116132-18-000021&xbrl_type=v#  
+Notes to Financial Statements - GOODWILL AND OTHER INTANGIBLE ASSETS - whole thing
+4. Tesla year 2019 and year 2018
+https://www.sec.gov/cgi-bin/viewer?action=view&cik=1318605&accession_number=0001564590-19-003165&xbrl_type=v# 
+ Notes to Financial Statements - Intangible Assets - whole thing
+https://www.sec.gov/cgi-bin/viewer?action=view&cik=1318605&accession_number=0001564590-18-002956&xbrl_type=v
+Notes to Financial Statements - Goodwill and Intangible Assets - whole thing 
 ## Run
 `python IntangibleAssets.py`
 ## Example
-See result.html for output.
+See tesla1.html and tesla2.html for sample output.
--- intangible.py
+++ intangible.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-url = 'https://www.sec.gov/cgi-bin/viewer?action=view&cik=1018724&accession_number=0001018724-19-000004&xbrl_type=v#'
+# url = 'https://www.sec.gov/cgi-bin/viewer?action=view&cik=1018724&accession_number=0001018724-19-000004&xbrl_type=v#'
+# url = 'https://www.sec.gov/cgi-bin/viewer?action=view&cik=789019&accession_number=0001564590-18-019062&xbrl_type=v#'
+# url = 'https://www.sec.gov/cgi-bin/viewer?action=view&cik=1318605&accession_number=0001564590-19-003165&xbrl_type=v#'
+url = 'https://www.sec.gov/cgi-bin/viewer?action=view&cik=1318605&accession_number=0001564590-18-002956&xbrl_type=v'
 
 # create a new Chrome session
 driver = webdriver.Chrome()
@@ -18,15 +21,15 @@ element.click()
 time.sleep(2)
 
 # Clik on the ACQUISITIONS, GOODWILL, AND ACQUIRED INTANGIBLE ASSETS link
-driver.find_element_by_xpath('//a[@href="javascript:loadReport(12);"]').click();
-
+# driver.find_element_by_xpath('//a[@href="javascript:loadReport(12);"]').click();
+try:
+	driver.find_element_by_partial_link_text('Intangible').click();
+except Exception as e:
+	driver.find_element_by_partial_link_text('INTANGIBLE').click();
 # Use BeautifulSoup to parse the DOM
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 # The whole html is a big table of table, the content is inside its td
 s = soup.find("table").find("table").find('tr', class_='ro').find('td', class_="text")
 
-# Intangible Assets is the 6th table inside this td
-Primarily_includes = s.findAll('table')[5]
-
-print(Primarily_includes)
+print(s)
